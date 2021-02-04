320bcff13fd149b1fd9faef9005ed70232a5b7dd
José Carneiro <contact@josecarnei.ro>
2021-02-04 16:20:02 +0000

    Adds sorting algorythm

--- routers/base.js
+++ routers/base.js
@@ -3,14 +3,41 @@ const Resource = require('./../models/resource');
 
 const router = new express.Router();
 
-router.get('/', (req, res) => {
-  Resource.find()
-    .populate('creator')
-    .then(resources => {
-      console.log(resources);
+router.get('/', (req, res, next) => {
+  // Inspired by https://jkchu.com/2016/02/17/designing-and-implementing-a-ranking-algorithm/
+  // This algorythm takes into account
+  // P - Points
+  // D - Creation date in milliseconds after epoch
+  // N - Now, number of milliseconds after epoch
+  // H - Number of milliseconds in 1h, 1000 * 60 * 60
+  // The final calculation is
+  // Ranking = (P + 1) ** 2 / ((N - D) / H)
+  Resource.aggregate([
+    {
+      $addFields: {
+        item: '$_id',
+        ranking: {
+          $divide: [
+            { $pow: [{ $sum: ['$points', 1] }, 2] },
+            {
+              $divide: [
+                { $subtract: [new Date(), '$creationDate'] },
+                1000 * 60 * 60
+              ]
+            }
+          ]
+        }
+      }
+    },
+    { $sort: { ranking: -1 } }
+  ])
+    .then((resources) => {
+      return Resource.populate(resources, 'creator');
+    })
+    .then((resources) => {
       res.render('home', { resources });
     })
-    .catch(error => {
+    .catch((error) => {
       next(error);
     });
 });
